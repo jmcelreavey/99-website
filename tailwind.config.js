78d177328e3a59a8649e6acb49a8e6b8b0dc0868
John <j.mcelreavey@gmail.com>
2021-06-09 23:24:03 +0100

    Playing

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+module.exports = {
+  mode: "jit",
+  purge: {
+    content: ["./app/**/*.{js,ts,jsx,tsx}"],
+    mode: "all",
+    preserveHtmlElements: false,
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        alegreya: ["Alegreya", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+      },
+      screens: {
+        "1-5xl": "1440px",
+        "2xl": "2560px",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/custom-forms")],
+}
